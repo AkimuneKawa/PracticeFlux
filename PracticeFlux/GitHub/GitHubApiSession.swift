fdcbd7363761174682f60c8018660766ccdb28c2
Kawa AKimune <akimune1003@gmail.com>
2021-01-08 21:50:36 +0900

    Create GitHubApiSession.swift

---
+++ PracticeFlux/GitHub/GitHubApiSession.swift
@@ -0,0 +1,14 @@
+//
+//  GitHubApiSession.swift
+//  PracticeFlux
+//
+//  Created by 河明宗 on 2021/01/08.
+//  Copyright © 2021 河明宗. All rights reserved.
+//
+
+import Foundation
+import GitHub
+
+protocol GitHubApiRequestable: AnyObject {
+    func searchRepository(query: String, page: Int, completion: @escaping (Result<([GitHub.Repository], GitHub.Pagination)>) -> ())
+}
