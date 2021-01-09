888aadb5782e941765c423acddc13b94757003ab
Kawa AKimune <akimune1003@gmail.com>
2021-01-09 19:22:24 +0900

    implement dispatcher

--- PracticeFlux/Flux/Dispatcher.swift
+++ PracticeFlux/Flux/Dispatcher.swift
@@ -8,6 +8,38 @@
 
 import Foundation
 
+typealias DispatchToken = String
+
 final class Dispatcher {
-    func dispatch(_ action: Action) {}
+    static let shared = Dispatcher()
+    
+    let lock: NSLocking
+    private var callbacks: [DispatchToken: (Action) -> ()]
+    
+    init() {
+        self.lock = NSRecursiveLock()
+        self.callbacks = [:]
+    }
+    
+    func register(callback: @escaping (Action) -> ()) -> DispatchToken {
+        lock.lock(); defer { lock.unlock() }
+        
+        let token = UUID().uuidString
+        callbacks[token] = callback
+        return token
+    }
+    
+    func unregister(_ token: DispatchToken) {
+        lock.lock(); defer { lock.unlock() }
+        
+        callbacks.removeValue(forKey: token)
+    }
+    
+    func dispatch(_ action: Action) {
+        lock.lock(); defer { lock.unlock() }
+        
+        callbacks.forEach { _, callback in
+            callback(action)
+        }
+    }
 }
