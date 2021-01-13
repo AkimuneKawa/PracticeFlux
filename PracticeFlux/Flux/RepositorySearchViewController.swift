d8478fac7c1d4f5ba05cde737191613d1ac1cc3c
Kawa AKimune <akimune1003@gmail.com>
2021-01-13 22:18:40 +0900

    add logic

--- PracticeFlux/Flux/RepositorySearchViewController.swift
+++ PracticeFlux/Flux/RepositorySearchViewController.swift
@@ -8,10 +8,26 @@
 
 import Foundation
 import UIKit
+import GitHub
 
 final class RepositorySearchViewController: UIViewController, UISearchBarDelegate {
+    private let tableView = UITableView()
+    private let searchController = UISearchController()
+    
     private let searchStore: SearchRepositoryStore
     private let actionCreator: ActionCreator
+    
+    private lazy var reloadSubscription: Subscription = {
+        return searchStore.addListner { [weak self] in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+    }()
+    
+    private var repositories: [Repository] {
+        return searchStore.repositories
+    }
     
     init(searchStore: SearchRepositoryStore = .shared, actionCreator: ActionCreator) {
         self.searchStore = searchStore
@@ -21,5 +37,28 @@ final class RepositorySearchViewController: UIViewController, UISearchBarDelegat
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        searchController.searchBar.delegate = self
+        _ = reloadSubscription
+    }
+    
+    private func setupConstraints() {
+        
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let text = searchBar.text, !text.isEmpty {
+            actionCreator.clearRepositories()
+            actionCreator.searchRepositories(query: text, page: 1)
+        }
     }
 }
