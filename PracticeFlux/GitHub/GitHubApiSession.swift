c049a73ea1662e831f01a6137910b4402ea305f8
Kawa AKimune <akimune1003@gmail.com>
2021-01-14 21:20:30 +0900

    create GitHubApiSession

--- PracticeFlux/GitHub/GitHubApiSession.swift
+++ PracticeFlux/GitHub/GitHubApiSession.swift
@@ -12,3 +12,22 @@ import GitHub
 protocol GitHubApiRequestable: AnyObject {
     func searchRepositories(query: String, page: Int, completion: @escaping (Result<([GitHub.Repository], GitHub.Pagination)>) -> ())
 }
+
+final class GitHubApiSession: GitHubApiRequestable {
+    static let shared = GitHubApiSession()
+    
+    private let session = GitHub.Session()
+    
+    func searchRepositories(query: String, page: Int, completion: @escaping (Result<([Repository], Pagination)>) -> ()) {
+        let request = SearchRepositoriesRequest(query: query, sort: .stars, order: .desc, page: page, perPage: nil)
+        
+        session.send(request, completion: { result in
+            switch result {
+            case let .success((response, pagination)):
+                completion(.success((response.items, pagination)))
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        })
+    }
+}
