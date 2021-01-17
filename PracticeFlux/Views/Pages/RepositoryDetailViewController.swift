73dcc6faa1631b81cc8e1f780eacb4eba0348571
Kawa AKimune <akimune1003@gmail.com>
2021-01-17 23:58:43 +0900

    create RepositoryDetailVC

---
+++ PracticeFlux/Views/Pages/RepositoryDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RepositoryDetailViewController.swift
+//  PracticeFlux
+//
+//  Created by 河明宗 on 2021/01/16.
+//  Copyright © 2021 河明宗. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class RepositoryDetailViewController: UIViewController {
+}
